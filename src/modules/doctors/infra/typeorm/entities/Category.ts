19e619999327effaa6f9b9101c5323604601200d
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-06-17 14:59:57 -0300

    typeorm decorators on category and entity

--- src/modules/doctors/infra/typeorm/entities/Category.ts
+++ src/modules/doctors/infra/typeorm/entities/Category.ts
@@ -1,10 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   symptomesAssociated: string[];
+
+  @CreateDateColumn()
   created_at?: Date;
+
+  @UpdateDateColumn()
   updated_at?: Date;
 
   constructor() {
--- src/modules/doctors/infra/typeorm/entities/Doctor.ts
+++ src/modules/doctors/infra/typeorm/entities/Doctor.ts
@@ -1,15 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToOne,
+  OneToOne,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { User } from "../../../../accounts/infra/typeorm/entities/User";
 import { Category } from "./Category";
 
+@Entity("doctors")
 class Doctor {
+  @OneToOne(() => User)
+  @JoinTable({ name: "user_id" })
   user_id: string;
+
+  @Column()
   crm: number;
+
+  @Column()
   clinicAdress: string;
+
+  @Column()
   availableAgenda: string;
+
+  @Column()
   rating: number;
+
+  @Column()
   ratingCount: number;
+
+  @Column()
   bio: string;
+
+  @ManyToOne(() => Category)
+  @JoinTable({ name: "category_id" })
   category: Category;
+
+  @CreateDateColumn()
   created_at?: Date;
+
+  @UpdateDateColumn()
   updated_at?: Date;
 
   constructor() {
