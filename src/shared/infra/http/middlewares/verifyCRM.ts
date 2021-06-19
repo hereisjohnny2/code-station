c590b1820034bf8e56a4e362039f0121e1400b5c
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-06-18 21:47:43 -0300

    crm number verification

---
+++ src/shared/infra/http/middlewares/verifyCRM.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { NextFunction, Request, Response } from "express";
+
+import { AppError } from "../../../errors/AppError";
+
+export async function verifyCRM(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const { crm, uf } = request.body;
+
+    const crmApiRequestRey = 4502488655;
+
+    const api = axios.create({
+      baseURL: "https://www.consultacrm.com.br/api/index.php",
+      params: {
+        tipo: "crm",
+        q: crm,
+        uf,
+        chave: crmApiRequestRey,
+        destino: "json",
+      },
+    });
+
+    const crmData = await api.get("/");
+
+    if (crmData.data.situacao !== "Ativo") {
+      throw new AppError("CRM is not active!");
+    }
+
+    next();
+  } catch (error) {
+    throw new AppError("Invalid CRM number!");
+  }
+}
--- src/shared/infra/http/routes/doctors.routes.ts
+++ src/shared/infra/http/routes/doctors.routes.ts
@@ -3,13 +3,19 @@ import { Router } from "express";
 import { CreateDoctorController } from "../../../../modules/doctors/useCases/createDoctorUseCase/CreateDoctorController";
 import { ShowDoctorProfileController } from "../../../../modules/doctors/useCases/showDoctorProfile/ShowDoctorProfileController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { verifyCRM } from "../middlewares/verifyCRM";
 
 const doctorsRoutes = Router();
 
 const createDcotorController = new CreateDoctorController();
 const showDoctorProfileController = new ShowDoctorProfileController();
 
-doctorsRoutes.post("/", ensureAuthenticated, createDcotorController.handle);
+doctorsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyCRM,
+  createDcotorController.handle
+);
 doctorsRoutes.get("/:user_id", showDoctorProfileController.handle);
 
 export { doctorsRoutes };
